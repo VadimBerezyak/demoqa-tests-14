c734dad306c7c7331927b68894ff7a04bda640d8
Berezyak Vadim <newberez@gmail.com>
2022-08-10 16:58:41 +0300

    RegistrationFormPage was added

---
+++ src/test/java/com/demoqa/pages/RegistrationFormPage.java
@@ -0,0 +1,14 @@
+package com.demoqa.pages;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.executeJavaScript;
+
+public class RegistrationFormPage {
+    public void openPage(){
+        open("/automation-practice-form");
+        $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
+        executeJavaScript("$('footer').remove()");
+        executeJavaScript("$('#fixedban').remove()");
+    }
+}
