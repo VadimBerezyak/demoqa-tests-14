e31013ced10db1f78a0517bafb4dc122deb1c95a
VadimBerezyak <89918671+VadimBerezyak@users.noreply.github.com>
2022-08-16 16:19:49 +0300

    testData in BeforeEach refactores

--- src/test/java/com/demoqa/tests/RegistrationFormWithTestDataTests.java
+++ src/test/java/com/demoqa/tests/RegistrationFormWithTestDataTests.java
@@ -2,6 +2,7 @@ package com.demoqa.tests;
 
 import com.demoqa.pages.RegistrationFormPage;
 import com.demoqa.pages.components.ResultsTableComponent;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static java.lang.String.format;
@@ -10,24 +11,44 @@ public class RegistrationFormWithTestDataTests extends TestBase {
     RegistrationFormPage registrationFormPage = new RegistrationFormPage();
     ResultsTableComponent resultsTableComponent = new ResultsTableComponent();
 
-    String firstName = "Alex",
-            lastName = "Egorov",
-            expectedFullName = format("%s %s", firstName, lastName),
-            email = "AlexEgorov@mail.com",
-            gender = "Male",
-            mobile = "1234567890",
-            subject = "Math",
-            hobbies = "Sport",
-            picturePath = "img/1.png",
-            currentAddress = "Some address 1",
-            state = "NCR",
-            city = "Delhi",
-            expectedStateCityAddress = format("%s %s", state, city),
-            day = "30",
-            month = "January",
-            year = "2000",
-            expectedDateOfBirth = format("%s %s,%s", day, month, year);
+    String firstName,
+            lastName,
+            expectedFullName,
+            email,
+            gender,
+            mobile,
+            subject,
+            hobbies,
+            picturePath,
+            currentAddress,
+            state,
+            city,
+            expectedStateCityAddress,
+            day,
+            month,
+            year,
+            expectedDateOfBirth;
 
+    @BeforeEach
+    void prepareTestData(){
+                firstName = "Alex";
+                lastName = "Egorov";
+                expectedFullName = format("%s %s", firstName, lastName);
+                email = "AlexEgorov@mail.com";
+                gender = "Male";
+                mobile = "1234567890";
+                subject = "Math";
+                hobbies = "Sport";
+                picturePath = "img/1.png";
+                currentAddress = "Some address 1";
+                state = "NCR";
+                city = "Delhi";
+                expectedStateCityAddress = format("%s %s", state, city);
+                day = "30";
+                month = "January";
+                year = "2000";
+                expectedDateOfBirth = format("%s %s,%s", day, month, year);
+    }
 
     @Test
     void fillFormTest() {
